93347d798de7756f2fe20dd80f748aee14fa879e
Kyle Oliveri <kyle@Kyles-MacBook-Air.local>
2021-05-02 12:47:03 -0700

    Layout, Header and Footer

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "421976392dbfbba2bd71"; }
+/******/ 		__webpack_require__.h = function() { return "cdff813d46a15aa06e38"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
